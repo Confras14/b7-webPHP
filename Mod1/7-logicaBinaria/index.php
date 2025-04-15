5c8c3c05a4ff0eb8e265c88eca64c03f8775949c
Lucas Conrado de Souza <lucas.c.souza14115@gmail.com>
2025-04-15 13:41:04 -0300

    Restruturando pastas para modulos, anotacoes de logica binaria (if e else) e Arrays

---
+++ Mod1/7-logicaBinaria/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$estagiario = true;
+
+var_dump($estagiario);
+echo '<br />';
+var_export($estagiario);
+echo '<br />';
+
+echo $estagiario;
+echo '<br />';
+
+if ($estagiario) {
+  echo "Sera que imprime?";
+} else {
+  echo "Acho que não!";
+}
+
+?>
---
+++ Mod1/8-Arrays/index.php
@@ -0,0 +1,56 @@
+<?php
+
+# Array normal
+$nomes = ["Lucas", "Pedro", "Julio"];
+
+for($i = 0; $i < 3; $i+=1) {
+  echo $nomes[$i].PHP_EOL;
+}
+
+echo '<br />';
+
+# Array Associativo
+
+$numeros = [
+  "um" => 1,
+  "dois" => 2,
+  "tres" => 3,
+  "quatro" => 4
+];
+
+echo $numeros["dois"] . '<br />';
+
+# Arrays multidimencionais 
+
+$pessoas = [
+  "Lucas" => [
+    "idade" => 19,
+    "carros" => 2,
+    "vivo" => true,
+    "nomeMae" => "Andreia"
+  ],
+
+  "Pedro" => [
+    "idade" => 20,
+    "carros" => 3,
+    "vivo" => true,
+    "nomeMae" => "Maria"
+  ],
+
+  "Carlos" => [
+    "idade" => 12,
+    "carros" => 0,
+    "vivo" => true,
+    "nomeMae" => "Julia"
+  ],
+];
+
+echo '<pre>'; // Tag HTML para deixar o texto preformatado
+  var_dump($pessoas); // Exibe a array (ja que o echo nao funciona)
+  echo "<br/>";
+  var_dump($pessoas["Lucas"]);
+  echo "<br/>";
+  var_dump($pessoas["Pedro"]["idade"]);
+echo '<pre/>';
+
+?>
